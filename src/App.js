451a3e1faae85d2f4981742cda4b8a6df6fb0ef7
ha0ha0 <gkdud6211@gmail.com>
2019-12-22 23:24:40 +0900

    food map coding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}) {
+  //console.log(props)
+  return(
+    <div>
+      <h2>love {name}</h2>
+      <img src={picture} />
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+    name: 'apple',
+    image: "https://cdn.pixabay.com/photo/2015/02/13/00/43/apples-634572_1280.jpg"
+  },
+  {
+    name: 'banana',
+    image: "https://cdn.pixabay.com/photo/2016/01/03/17/59/bananas-1119790_1280.jpg"
+  },
+  {
+    name: 'lime',
+    image:"https://cdn.pixabay.com/photo/2017/03/10/15/15/lime-2133091_1280.jpg",
+  },
+  {
+    name: 'orange',
+    image:"https://cdn.pixabay.com/photo/2016/10/07/14/11/tangerines-1721633_1280.jpg"
+  }
+]
+
+function renderFood(){
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
